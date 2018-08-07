f23bf73516b11d4a19c3ae9f117598b9134ac947
Maynor <mynorlarios.ec10@gmail.com>
2018-08-06 19:11:10 -0600

    actualizacion a mongo

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = require('./app');
 
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/zoo');
-mongoose.connect('mongodb://mlarios10:ElectroCompu10@ds131971.mlab.com:31971/backend-2018');
+mongoose.connect('mongodb://mlarios11:electronica10@ds131971.mlab.com:31971/backend-2018');
 app.listen(port);
 
 console.log('El servidor local de node y express esta corriendo');
@@ -16,4 +16,4 @@ console.log('El servidor local de node y express esta corriendo');
      //       console.log('El servidor local de node y express esta corriendo');
      //   });
     //})
-    //.catch(err => console.log(err));
+    //.catch(err => console.log(err)nkk
