0fbb8538f19b4eda16f7ce5ba2d693a3aadccff5
Maynor <mynorlarios.ec10@gmail.com>
2018-08-08 18:40:05 -0600

    actualizado 8 de agosto

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ var app = require('./app');
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/zoo');
 mongoose.connect('mongodb://mlarios11:electronica10@ds131971.mlab.com:31971/backend-2018');
+
 app.listen(port);
 
-console.log('El servidor local de node y express esta corriendo');
+console.log('backend is running');
+
    // .then(() => {
    //     console.log('La conexion a mongo a sido exitosa');
     //    app.listen(port, () => {
